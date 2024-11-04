0d23f67ec92fe9f89cfced76c4e2faa687bcbd67
delale@MacBookPro <alessandro.deluca@uzh.ch>
2024-11-04 11:12:28 +0100

    kickstart.nvim -> zaibatsu as default colorscheme

--- init.lua
+++ init.lua
@@ -768,9 +768,9 @@ require('lazy').setup({
         -- No, but seriously. Please read `:help ins-completion`, it is really good!
         mapping = cmp.mapping.preset.insert {
           -- Select the [n]ext item
-          ['<C-n>'] = cmp.mapping.select_next_item(),
+          -- ['<C-n>'] = cmp.mapping.select_next_item(),
           -- Select the [p]revious item
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          -- ['<C-p>'] = cmp.mapping.select_prev_item(),
 
           -- Scroll the documentation window [b]ack / [f]orward
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -779,13 +779,13 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
-          --['<CR>'] = cmp.mapping.confirm { select = true },
-          --['<Tab>'] = cmp.mapping.select_next_item(),
-          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
@@ -846,16 +846,16 @@ require('lazy').setup({
   --    end,
   --  },
 
-  --  { -- Catppuccin colorscheme
-  --    'catppuccin/nvim',
-  --    name = 'catppuccin',
-  --    priority = 1000,
-  --    init = function()
-  --      -- Load colorscheme here.
-  --      vim.cmd.colorscheme 'catppuccin'
-  --    end,
-  --  },
-
+  { -- Catppuccin colorscheme
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    init = function()
+      -- Load colorscheme here.
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  --
   { -- Monokai-pro colorscheme
     'loctvl842/monokai-pro.nvim',
     name = 'monokai-pro',
@@ -864,7 +864,7 @@ require('lazy').setup({
       vim.cmd.colorscheme 'monokai-pro'
     end,
   },
-
+  --
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
@@ -974,6 +974,9 @@ require('lazy').setup({
     },
   },
 })
+
+-- default colorscheme
+vim.cmd.colorscheme 'zaibatsu'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
