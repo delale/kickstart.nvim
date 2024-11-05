7630586355246727e0d85bd85023ed2e1b46db4e
delale@MacBookPro <alessandro.deluca@uzh.ch>
2024-11-05 08:24:09 +0100

    monokai as default colorscheme

--- init.lua
+++ init.lua
@@ -975,8 +975,5 @@ require('lazy').setup({
   },
 })
 
--- default colorscheme
-vim.cmd.colorscheme 'zaibatsu'
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
